ded3f6a6fa153c5428fb3dd21771fc4c59e9ea89
Ziyao Chen <zc2717@nyu.edu>
2026-04-10 09:51:08 +0400

    ignore and remove scripts/docs

--- run_full_train_lift3d.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# LIFT3D 全量训练（Stage1->2->3->4）。可覆盖: DATA, LIFT3D_ROOT, GRASPNET_BASELINE, BATCH_SIZE
-set -e
-cd "$(dirname "$0")"
-export MODE=full
-exec bash run_train_adapter_lift3d.sh
